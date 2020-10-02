f0df074bd214db8b2ebed271731104281249f0b0
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 16:51:42 +0800

    >  compile NEMU 3019244222 yhy Linux yhy-VirtualBox 5.4.0-48-generic #52-Ubuntu SMP Thu Sep 10 10:58:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:51:42 up  1:14,  1 user,  load average: 0.02, 0.05, 0.07 82eebfc7c8ea3546701eac432bd6e4d8a007dcbf

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -77,10 +77,9 @@ static int cmd_x(char *args) {
 	sscanf(args,"%x",&address);
 	printf("0x%08x:",address);
 	for(i = 0;i < num;i++) {
-		printf("0x%08x",swaddr_read(address,4));
+		printf("0x%08x\n",swaddr_read(address,4));
 		address = address + 4;
 	}
-	printf("\n");
 	return 0;
 }
 
