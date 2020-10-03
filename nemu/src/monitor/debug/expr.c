02e652de3736a984875e368f7e98e78b923c0973
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 18:43:48 +0800

    >  compile NEMU 3019244222 yhy Linux yhy-VirtualBox 5.4.0-48-generic #52-Ubuntu SMP Thu Sep 10 10:58:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:43:48 up  4:09,  1 user,  load average: 0.08, 0.14, 0.09 d6762093d2036ed932e3999aa602c3691bfa3920

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -177,8 +177,8 @@ uint32_t eval(int p ,int q) {
 				case '!' : return !tmp;
 			}
 		}
-		int val1 = eval(p,op - 1);
-		int val2 = eval(op + 1,q);
+		u_int32_t val1 = eval(p,op - 1);
+		u_int32_t val2 = eval(op + 1,q);
 		switch(tokens[op].type) {
 			case '+': return val1 + val2;
 			case '-': return val1 - val2;
