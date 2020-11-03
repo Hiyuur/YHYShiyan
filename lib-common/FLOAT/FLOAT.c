c514b1a5276877251804a1e2062ebfd85fe0c85e
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 23:42:37 +0800

    >  compile NEMU 3019244222 yhy Linux yhy-VirtualBox 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:42:37 up  3:08,  1 user,  load average: 0.40, 0.17, 0.07 c64d89516a5c6fa2b4214218b766b09c7885e171

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,8 +1,9 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+	long long temp = (long long)a * (long long)b;
+	FLOAT result = (FLOAT)(temp >> 16);
+	return result;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
@@ -23,9 +24,34 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 	 * It is OK not to use the template above, but you should figure
 	 * out another way to perform the division.
 	 */
-
-	nemu_assert(0);
-	return 0;
+	int sign;
+	if(a < 0) {
+		sign = -1;
+		a = -a;
+	}
+	else {
+		sign = 1;
+	}
+	if(b < 0) {
+		sign = -1;
+		b = -b;
+	}
+	else {
+		sign = 1;
+	}
+	int result = a / b;
+	a = a % b;
+	int i;
+	for (i = 0; i < 16; i++) {
+		a = a << 1;
+		result = result << 1;
+		if (a >= b) {
+			a = a - b;
+			result++;
+		}
+	}
+	return result * sign;
+	
 }
 
 FLOAT f2F(float a) {
@@ -39,13 +65,38 @@ FLOAT f2F(float a) {
 	 * performing arithmetic operations on it directly?
 	 */
 
-	nemu_assert(0);
-	return 0;
+	void *temp = &a;
+	int val = * (int*) temp;
+	int sign = val >> 31;
+	int exp = (val >> 23) & 0xff;
+	FLOAT result = val &0x7fffff;
+	if(exp != 0) {
+		result = result + (1 << 23);
+	} 
+	exp -= 150;
+	if(exp < -16) {
+		result = result >> (-16 - exp);
+	}
+	if(exp > -16) {
+		result = result << (exp + 16);
+	}
+	if(sign == 0) {
+		return result;
+	}
+	else {
+		return -result;
+	}
 }
 
 FLOAT Fabs(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	FLOAT b;
+	if(a < 0) {
+		b = - a;
+	}
+	else {
+		b = a;
+	}
+	return b;
 }
 
 /* Functions below are already implemented */
